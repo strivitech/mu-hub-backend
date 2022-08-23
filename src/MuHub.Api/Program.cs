847ab70842a20b3def3c189bbea5790c3aa6b129
provicevko <provicevk@gmail.com>
2022-08-23 12:53:18 +0300

    Added Sonar test comment

--- src/MuHub.Api/Program.cs
+++ src/MuHub.Api/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Here sonar PR test
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
