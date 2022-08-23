b660b38ee53c90ddfd9c301981dad56f20899594
provicevko <provicevk@gmail.com>
2022-08-23 13:14:52 +0300

    Test sonar cloud

---
+++ src/MuHub.Api/clas.cs
@@ -0,0 +1,13 @@
+﻿namespace MuHub.Api;
+
+public class clas
+{
+    public string _a { get; set; } = "";
+
+    public void Baba()
+    {
+        string s = "rfghdrfhe";
+        
+        Console.WriteLine("q");
+    }
+}
