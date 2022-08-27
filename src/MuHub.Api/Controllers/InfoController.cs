e0f21904a201ab3b77a69dfa206a895570aca737
strivitech <strivitech@gmail.com>
2022-08-28 01:58:55 +0300

    Test duplication

--- src/MuHub.Api/Controllers/InfoController.cs
+++ src/MuHub.Api/Controllers/InfoController.cs
@@ -18,3 +18,39 @@ public class InfoController : ControllerBase
         return Ok();
     }
 }
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class InfoController2 : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    public IActionResult Index2()
+    {
+        return Ok();
+    }
+}
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class InfoController3 : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var s = "String";
+        return Ok(s);
+    }
+    
+    [HttpGet]
+    public IActionResult Index2()
+    {
+        var s = "String";
+        return Ok();
+    }
+}
