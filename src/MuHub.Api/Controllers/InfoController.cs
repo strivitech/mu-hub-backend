c2e2984ea70d0a9fe1fbe855655d683c32fce9a1
strivitech <strivitech@gmail.com>
2022-08-28 02:11:14 +0300

    Removed unnecessary code

--- src/MuHub.Api/Controllers/InfoController.cs
+++ src/MuHub.Api/Controllers/InfoController.cs
@@ -11,46 +11,4 @@ public class InfoController : ControllerBase
     {
         return Ok();
     }
-    
-    [HttpGet]
-    public IActionResult Index2()
-    {
-        return Ok();
-    }
-}
-
-[ApiController]
-[Route("[controller]/[action]")]
-public class InfoController2 : ControllerBase
-{
-    [HttpGet]
-    public IActionResult Index()
-    {
-        return Ok();
-    }
-    
-    [HttpGet]
-    public IActionResult Index2()
-    {
-        return Ok();
-    }
-}
-
-[ApiController]
-[Route("[controller]/[action]")]
-public class InfoController3 : ControllerBase
-{
-    [HttpGet]
-    public IActionResult Index()
-    {
-        var s = "String";
-        return Ok(s);
-    }
-    
-    [HttpGet]
-    public IActionResult Index2()
-    {
-        var s = "String";
-        return Ok(s);
-    }
 }
