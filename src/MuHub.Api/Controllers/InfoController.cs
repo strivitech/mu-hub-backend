799327a1f44f3a24ac7627f0db6389c5140aea84
strivitech <111649007+strivitech@users.noreply.github.com>
2022-08-28 21:34:57 +0300

    Added test workflow (#4)
    * Added test workflow
    
    * Fixed relative path
    
    * Added dotnet coverlet.console to workflow
    
    * Changed coverlet logic
    
    * Added comment to first test
    
    * Added InfoController
    
    * Added test for InfoController
    
    * Added duplicate
    
    * Removed slash
    
    * Test duplication
    
    * Duplicate test #2
    
    * Removed unnecessary code
    
    * EditorConfig new lines changes
    
    Co-authored-by: provicevko <provicevk@gmail.com>

---
+++ src/MuHub.Api/Controllers/InfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MuHub.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class InfoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
---
+++ tests/UnitTests/MuHub.Api.Tests/Controllers/InfoControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MuHub.Api.Controllers;
+
+namespace MuHub.Api.Tests.Controllers;
+
+public class InfoControllerTest
+{
+    private readonly InfoController _infoController;
+
+    public InfoControllerTest()
+    {
+        _infoController = new InfoController();
+    }
+
+    [Fact]
+    public void Index_Test()
+    {
+        // Act
+        var result = _infoController.Index();
+
+        // Assert
+        Assert.IsType<OkResult>(result);
+    }
+}
