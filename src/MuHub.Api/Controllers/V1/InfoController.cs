3a0dc5ae7f8bcc84cb03399aac0884a5eeb5824b
strivitech <strivitech@gmail.com>
2022-09-02 15:00:00 +0300

    Fix controller

--- src/MuHub.Api/Controllers/V1/InfoController.cs
+++ src/MuHub.Api/Controllers/V1/InfoController.cs
@@ -2,6 +2,9 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+using MuHub.Application.Models.Requests.Info;
+using MuHub.Application.Services.Interfaces;
+
 namespace MuHub.Api.Controllers.V1;
 
 [ApiController]
@@ -9,9 +12,24 @@ namespace MuHub.Api.Controllers.V1;
 [Route("api/v{version:apiVersion}/[controller]")]
 public class InfoController : ControllerBase
 {
+    private readonly IInfoService _infoService;
+
+    public InfoController(IInfoService infoService)
+    {
+        _infoService = infoService ?? throw new ArgumentNullException(nameof(infoService));
+    }
+    
     [HttpGet]
     public IActionResult Index()
     {
         return Ok("V1");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateInfoRequest request)
+    {
+        await _infoService.CreateAsync(request);
+
+        return Ok();
     }
 }
--- tests/UnitTests/MuHub.Api.Tests/Controllers/InfoControllerTest.cs
+++ tests/UnitTests/MuHub.Api.Tests/Controllers/InfoControllerTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Moq;
+
 using MuHub.Api.Controllers.V1;
+using MuHub.Application.Services.Interfaces;
 
 namespace MuHub.Api.Tests.Controllers;
 
