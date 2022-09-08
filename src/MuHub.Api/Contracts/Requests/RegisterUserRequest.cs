c324b89e4d0e0ac980a9de7de98c69317b920cc1
strivitech <strivitech@gmail.com>
2022-09-09 00:40:29 +0300

    Added request models

---
+++ src/MuHub.Api/Contracts/Requests/RegisterUserRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MuHub.Api.Contracts.Requests;
+
+/// <summary>
+/// Request model for user registration.
+/// </summary>
+public class RegisterUserRequest
+{
+    /// <summary>
+    /// User login.
+    /// </summary>
+    public string Login { get; set; } = null!;
+    
+    /// <summary>
+    /// User Password.
+    /// </summary>
+    public string Password { get; set; } = null!;
+    
+    /// <summary>
+    /// Repeat User Password.
+    /// </summary>
+    public string PhoneNumber { get; set; } = null!;
+    
+    /// <summary>
+    /// Repeat User Password.
+    /// </summary>
+    [Compare(nameof(Password))]
+    public string RepeatPassword { get; set; } = null!;
+}
---
+++ src/MuHub.Infrastructure/Contracts/Requests/CreateUserRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MuHub.Infrastructure.Contracts.Requests;
+
+public class CreateUserRequest
+{
+    /// <summary>
+    /// User login.
+    /// </summary>
+    public string Login { get; set; } = null!;
+    
+    /// <summary>
+    /// User Password.
+    /// </summary>
+    public string Password { get; set; } = null!;
+    
+    /// <summary>
+    /// Phone number.
+    /// </summary>
+    public string PhoneNumber { get; set; } = null!;
+    
+    public string RoleName { get; set; } = null!;
+}
