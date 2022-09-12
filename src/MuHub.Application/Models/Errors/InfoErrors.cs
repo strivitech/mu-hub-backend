445a546377ced6943f3b635b5874f01f80563d74
strivitech <strivitech@gmail.com>
2022-09-13 00:46:47 +0300

    Added Errors class, describing all inner errors if exceptions are inappropriate

---
+++ src/MuHub.Application/Models/Errors/InfoErrors.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+namespace MuHub.Application.Models.Errors;
+
+/// <summary>
+/// Here we define the error codes that are used in the application.
+/// </summary>
+public static partial class Errors
+{
+    /// <summary>
+    /// Informational errors.
+    /// </summary>
+    public static class Info
+    {
+        /// <summary>
+        /// The operation was not successful because of invalid input.
+        /// </summary>
+        public static Error InvalidInput => 
+            Error.Validation("Info.InvalidInput", "Invalid input");
+    }
+}
