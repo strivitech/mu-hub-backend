1529e350f1c619f9ddb05937bf142c9128bb2938
strivitech <strivitech@gmail.com>
2022-09-15 00:58:08 +0300

    Changed Error action method implementation

--- src/MuHub.Api/Controllers/ErrorController.cs
+++ src/MuHub.Api/Controllers/ErrorController.cs
@@ -2,37 +2,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
+using MuHub.Api.Common.Constants;
 using MuHub.Application.Exceptions;
 
 namespace MuHub.Api.Controllers;
 
+/// <summary>
+/// Error controller that is used to handle all unhandled exceptions.
+/// </summary>
 public class ErrorController : ControllerBase
 {
+    /// <summary>
+    /// Handles all unhandled exceptions.
+    /// </summary>
+    /// <returns></returns>
     [Route("/error")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public IActionResult Error()
     {
         var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
 
-        if (exception is not null)
+        if (exception is DomainException domainException)
         {
-            if (exception is ValidationException validationException)
+            if (exception is ModelValidationException validationException)
             {
                 return ValidationProblem(validationException);
             }
             
-            var statusCode = RetrieveStatusCode(exception);
-            
-            return Problem(statusCode: statusCode);
+            return Problem(domainException);
         }
-        
+
         return Problem();
     }
-
-    private IActionResult ValidationProblem(ValidationException validationException)
+    
+    private IActionResult Problem(DomainException exception)
+    {
+        if (exception.AdditionalContext is not null)
+        {
+            HttpContext.Items[ProblemDetailsConstants.AdditionalContext] = exception.AdditionalContext;
+        }
+        
+        var statusCode = RetrieveStatusCode(exception);
+            
+        return Problem(statusCode: statusCode, detail: exception.Message);
+    }
+    
+    private IActionResult ValidationProblem(ModelValidationException modelValidationException)
     {
         var modelStateDictionary = new ModelStateDictionary();
                 
-        foreach (var validationExceptionError in validationException.Errors)
+        foreach (var validationExceptionError in modelValidationException.Errors)
         {
             modelStateDictionary.AddModelError(validationExceptionError.Key, validationExceptionError.Value.First());
         }
