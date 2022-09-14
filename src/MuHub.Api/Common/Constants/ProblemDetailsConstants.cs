8b8cb2a27056db1799f0fa433c71063fcceefe75
strivitech <strivitech@gmail.com>
2022-09-15 00:57:33 +0300

    Added a custom ProblemDetailsFactory implementation that includes additional context

---
+++ src/MuHub.Api/Common/Constants/ProblemDetailsConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace MuHub.Api.Common.Constants;
+
+/// <summary>
+/// Contains constants for problem details.
+/// </summary>
+public class ProblemDetailsConstants
+{
+    /// <summary>
+    /// AdditionalContext key.
+    /// </summary>
+    public const string AdditionalContext = "additionalContext";
+}
