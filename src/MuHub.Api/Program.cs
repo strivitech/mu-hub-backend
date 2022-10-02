c3aceab4ad79db061953a193ed945471d7add98f
strivitech <strivitech@gmail.com>
2022-10-03 00:09:56 +0300

    Add default Role

--- src/MuHub.Api/Program.cs
+++ src/MuHub.Api/Program.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Identity;
+
 using MuHub.Api;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +18,19 @@ app.Configure();
 try
 {
     // Add logs
+    // TODO: move this code
+    using var scope = app.Services.CreateScope();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    if (!roleManager.Roles.Any())
+    {
+        roleManager.CreateAsync(new IdentityRole("user")).Wait();
+    }
     app.Run();
 }
 catch (Exception ex)
 {
     // Add logs
+    Debug.WriteLine(JsonSerializer.Serialize(ex));
 }
 finally
 {
