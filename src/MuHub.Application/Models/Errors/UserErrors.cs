50f8aa58f8f4063a077895afade773bd812eb422
strivitech <strivitech@gmail.com>
2022-10-03 00:10:12 +0300

    Add User errors

---
+++ src/MuHub.Application/Models/Errors/UserErrors.cs
@@ -0,0 +1,32 @@
+﻿using ErrorOr;
+
+namespace MuHub.Application.Models.Errors;
+
+public static partial class Errors
+{
+    public class User
+    {
+        public static Error NotFound => Error.NotFound("User.NotFound", "User not found");
+        public static Error AlreadyExists => Error.Conflict("User.AlreadyExists", "User already exists");
+        public static Error NotActive => Error.Failure("User.NotActive", "User is not active");
+        public static Error CreationFailed => Error.Failure("User.CreationFailed", "User was not created because of errors");
+        public static Error AddToRoleFailed => Error.Failure("User.AddToRoleFailed", "User was not created because of errors");
+        public static Error NotConfirmed => Error.Failure("User.NotConfirmed", "User is not confirmed");
+        public static Error IsLockedOut => Error.Failure("User.IsLockedOut", "User is locked out");
+        public static Error IsDeleted => Error.Failure("User.IsDeleted", "User is deleted");
+        public static Error IsDisabled => Error.Failure("User.IsDisabled", "User is disabled");
+        public static Error IsNotInRole => Error.Failure("User.IsNotInRole", "User is not in role");
+        public static Error IsInRole => Error.Failure("User.IsInRole", "User is in role");
+        public static Error IsNotInAnyRole => Error.Failure("User.IsNotInAnyRole", "User is not in any role");
+        public static Error IsInAnyRole => Error.Failure("User.IsInAnyRole", "User is in any role");
+        public static Error IsNotInAllRoles => Error.Failure("User.IsNotInAllRoles", "User is not in all roles");
+        public static Error IsInAllRoles => Error.Failure("User.IsInAllRoles", "User is in all roles");
+        public static Error IsNotInAnyOfRoles => Error.Failure("User.IsNotInAnyOfRoles", "User is not in any of roles");
+        public static Error IsInAnyOfRoles => Error.Failure("User.IsInAnyOfRoles", "User is in any of roles");
+        public static Error IsNotInAllOfRoles => Error.Failure("User.IsNotInAllOfRoles", "User is not in all of roles");
+        public static Error IsInAllOfRoles => Error.Failure("User.IsInAllOfRoles", "User is in all of roles");
+        public static Error IsNotInAnyOfRolesList => Error.Failure("User.IsNotInAnyOfRolesList", "User is not in any of roles list");
+        public static Error IsInAnyOfRolesList => Error.Failure("User.IsInAnyOfRolesList", "User is in any of roles list");
+    }
+}
+
