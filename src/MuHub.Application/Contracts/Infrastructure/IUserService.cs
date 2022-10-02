1142b2332ad4d4c7eb824fd7bf5e3c4bf9d6c60a
strivitech <strivitech@gmail.com>
2022-10-03 00:09:24 +0300

    Add User DTO

---
+++ src/MuHub.Application/Contracts/Infrastructure/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+
+using MuHub.Application.Models.Data;
+using MuHub.Application.Models.Requests.User;
+
+namespace MuHub.Application.Contracts.Infrastructure;
+
+public interface IUserService
+{
+    Task<ErrorOr<UserDto>> CreateUserAsync(CreateUserRequest request);
+}
