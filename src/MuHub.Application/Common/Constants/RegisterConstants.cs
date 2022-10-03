5d918aabfafb25013cff3709a6846f7d9847b980
strivitech <strivitech@gmail.com>
2022-10-03 23:07:40 +0300

    Add constants for the registration process

---
+++ src/MuHub.Application/Common/Constants/RegisterConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace MuHub.Application.Common.Constants;
+
+public static class RegisterConstants
+{
+    public const string PasswordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+}
