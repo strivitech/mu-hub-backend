f335347721cf27994e41d8404ce3b2663f807c8f
strivitech <strivitech@gmail.com>
2022-10-04 23:31:22 +0300

    Add model validation factory

---
+++ src/MuHub.Api/Common/Factories/CustomModelValidationFactory.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+
+namespace MuHub.Api.Common.Factories;
+
+/// <summary>
+/// 
+/// </summary>
+public class CustomModelValidationFactory : ICustomModelValidationFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    public CustomModelValidationFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public IValidator? GetValidator(Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+        
+        var genericValidatorType = typeof(IValidator<>);
+        var specificValidatorType = genericValidatorType.MakeGenericType(type);
+        
+        using var scope = _serviceProvider.CreateScope();
+        var validatorInstance = (IValidator?) scope.ServiceProvider.GetService(specificValidatorType);
+        return validatorInstance;
+    }
+}
---
+++ src/MuHub.Api/Common/Factories/ICustomModelValidationFactory.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MuHub.Api.Common.Factories;
+
+/// <summary>
+/// 
+/// </summary>
+public interface ICustomModelValidationFactory
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    IValidator? GetValidator(Type type);
+}
