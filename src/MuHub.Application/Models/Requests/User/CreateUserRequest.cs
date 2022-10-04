89e9dc95d84b61632b9ac7a8a1d4bef5e1b6732d
strivitech <strivitech@gmail.com>
2022-10-04 18:40:48 +0300

    Remove unnecessary attributes

--- src/MuHub.Application/Models/Requests/User/CreateUserRequest.cs
+++ src/MuHub.Application/Models/Requests/User/CreateUserRequest.cs
@@ -9,22 +9,16 @@ public class CreateUserRequest
     /// <summary>
     /// User login.
     /// </summary>
-    [StringLength(25, MinimumLength = 3)]
-    [EmailAddress]
     public string Login { get; set; } = null!;
 
     /// <summary>
     /// User Password.
     /// </summary>
-    [RegularExpression(
-        RegisterConstants.PasswordRegex,
-        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.")]
     public string Password { get; set; } = null!;
 
     /// <summary>
     /// Phone number.
     /// </summary>
-    [Phone]
     public string PhoneNumber { get; set; } = null!;
     
     public string RoleName { get; set; } = null!;
