57ad3f602011cd373483af0c493d938074c0f20a
strivitech <strivitech@gmail.com>
2022-10-09 14:10:41 +0300

    Add user constants

---
+++ src/MuHub.Shared/Constants/Domain/UserConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace MuHub.Shared.Constants.Domain;
+
+/// <summary>
+/// Contains constants for the user.
+/// </summary>
+public static class UserConstants
+{
+    /// <summary>
+    /// Login length.
+    /// </summary>
+    public const int LoginLength = 50;
+    
+    /// <summary>
+    /// Regex for the password.
+    /// </summary>
+    public const string PasswordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+
+    /// <summary>
+    /// Regex for the phone. Contains 6-14 numbers from 0-9 range, doesn't include + sign.
+    /// </summary>
+    public const string PhoneNumberRegex = @"^[0-9]{6,14}$";
+    
+    /// <summary>
+    /// Default user role.
+    /// </summary>
+    public const string DefaultRoleName = "user";
+}
