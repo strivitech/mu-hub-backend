51f0a0cee52999ca12a79f8e09feea654fa5aee8
strivitech <strivitech@gmail.com>
2022-12-13 23:01:12 +0200

    Add linking mechanism for user Registration Flow

---
+++ src/MuHub.Api/Controllers/V1/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MuHub.Api.Requests;
+using MuHub.Api.Services;
+
+namespace MuHub.Api.Controllers.V1;
+
+/// <summary>
+/// 
+/// </summary>
+[ApiController]
+[ApiVersion(1.0)]
+[Route("api/v{version:apiVersion}/[controller]/[action]")]
+public class AuthController : MuControllerBase
+{
+    private readonly IAuthService _authService;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="authService"></param>
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> RelateUserRegistrationFlow(UserLinkRegistrationDataRequest request)
+    {
+        var userLinkResponse = await _authService.RelateUserRegistrationFlowAsync(request);
+        return Ok(userLinkResponse);
+    }
+}
---
+++ src/MuHub.Api/Services/AuthService.cs
@@ -0,0 +1,70 @@
+﻿using Amazon.CognitoIdentityProvider;
+
+using AutoMapper;
+
+using MuHub.Api.Requests;
+using MuHub.Api.Responses;
+using MuHub.Application.Contracts.Infrastructure;
+using MuHub.Application.Contracts.Persistence;
+using MuHub.Application.Services.Interfaces;
+using MuHub.Domain.Entities;
+
+namespace MuHub.Api.Services;
+
+/// <summary>
+/// 
+/// </summary>
+public class AuthService : IAuthService
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IIdentityProvider _identityProvider;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
+    private readonly IModelValidationService _modelValidationService;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <param name="identityProvider"></param>
+    /// <param name="mapper"></param>
+    /// <param name="logger"></param>
+    /// <param name="modelValidationService"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public AuthService(IApplicationDbContext dbContext, IIdentityProvider identityProvider, IMapper mapper,
+        ILogger logger, IModelValidationService modelValidationService)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        _identityProvider = identityProvider ?? throw new ArgumentNullException(nameof(identityProvider));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _modelValidationService = modelValidationService ?? throw new ArgumentNullException(nameof(modelValidationService));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public async Task<UserLinkRegistrationDataResponse?> RelateUserRegistrationFlowAsync(
+        UserLinkRegistrationDataRequest request)
+    {
+        var user = _dbContext.Users
+            .FirstOrDefault(x => x.UserName == request.UserName);
+        if (user is not null)
+        {
+            return new UserLinkRegistrationDataResponse { ApplicationUserId = user.Id };
+        }
+
+        var identityUser = await _identityProvider.GetIdentityProviderUserAsync(request.UserName);
+        var identityUserValid = await _modelValidationService.CheckIfValidAsync(identityUser);
+        if (!identityUserValid)
+        {
+            return null;
+        }
+
+        var newUserEntry = await _dbContext.Users.AddAsync(_mapper.Map<User>(identityUser));
+        await _dbContext.SaveChangesAsync();
+        return new UserLinkRegistrationDataResponse { ApplicationUserId = newUserEntry.Entity.Id };
+    }
+}
