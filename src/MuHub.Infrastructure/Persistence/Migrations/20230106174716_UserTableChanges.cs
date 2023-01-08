a17d41994a9276716638ee8e4f1fbffce5603d49
strivitech <strivitech@gmail.com>
2023-01-08 03:27:48 +0200

    Add new Db migration

---
+++ src/MuHub.Infrastructure/Persistence/Migrations/20230106174716_UserTableChanges.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MuHub.Infrastructure.Persistence.Migrations
+{
+    public partial class UserTableChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityProviderId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityProviderId",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastLoginAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
