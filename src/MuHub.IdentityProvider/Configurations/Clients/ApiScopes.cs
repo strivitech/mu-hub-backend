c8332482e919fa120b2e41de1efeea6c34e1b14e
strivitech <strivitech@gmail.com>
2023-01-10 23:11:20 +0200

    Add ApiScopes constants

---
+++ src/MuHub.IdentityProvider/Configurations/Clients/ApiScopes.cs
@@ -0,0 +1,8 @@
+﻿namespace MuHub.IdentityProvider.Configurations.Clients;
+
+public static class ApiScopes
+{
+    public const string MuHubApiRead= "muhubapi.read";
+    public const string MuHubApiWrite= "muhubapi.write";
+    public const string MuHubApiDelete= "muhubapi.delete";
+}
