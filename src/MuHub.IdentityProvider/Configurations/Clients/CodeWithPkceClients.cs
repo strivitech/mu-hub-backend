6c5f980f0075de6c9b9ad4078461159b7068dcff
strivitech <strivitech@gmail.com>
2023-01-10 23:12:37 +0200

    Add classes for clients' configurations

---
+++ src/MuHub.IdentityProvider/Configurations/Clients/CodeWithPkceClients.cs
@@ -0,0 +1,10 @@
+﻿namespace MuHub.IdentityProvider.Configurations.Clients;
+
+public class CodeWithPkceClients
+{
+    public string ClientId { get; set; }
+    public string[] RedirectUris { get; set; }
+    public string[] PostLogoutRedirectUris { get; set; }
+    public string[] AllowedCorsOrigins { get; set; }
+    public string[] AllowedScopes { get; set; }
+}
---
+++ src/MuHub.IdentityProvider/Configurations/Clients/IdentityServerClientsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace MuHub.IdentityProvider.Configurations.Clients;
+
+public class IdentityServerClientsConfiguration
+{
+    public const string SectionName = "IdentityServer:Clients";
+    
+    public CodeWithPkceClients[] CodeWithPkce { get; set; }
+}
