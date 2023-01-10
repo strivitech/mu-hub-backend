e18926db80ad594848c9081fdbd41f9fa6a2fc11
strivitech <strivitech@gmail.com>
2023-01-10 03:14:29 +0200

    Redirect allow non-local uris

--- src/MuHub.IdentityProvider/Pages/Redirect/Index.cshtml.cs
+++ src/MuHub.IdentityProvider/Pages/Redirect/Index.cshtml.cs
@@ -11,11 +11,6 @@ public class IndexModel : PageModel
 
     public IActionResult OnGet(string redirectUri)
     {
-        if (!Url.IsLocalUrl(redirectUri))
-        {
-            return RedirectToPage("/Home/Error/Index");
-        }
-
         RedirectUri = redirectUri;
         return Page();
     }
