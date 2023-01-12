40d06a872e888a6e6132c1318da76eaa89f5fb95
strivitech <strivitech@gmail.com>
2023-01-13 00:55:58 +0200

    Add services containing information about current user session

---
+++ src/MuHub.Api/Services/CurrentUserService.cs
@@ -0,0 +1,24 @@
+﻿using MuHub.Application.Contracts.Infrastructure;
+
+namespace MuHub.Api.Services;
+
+/// <summary>
+/// 
+/// </summary>
+public class CurrentUserService : ICurrentUserService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="userSessionData"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public CurrentUserService(IUserSessionData userSessionData)
+    {
+        UserSessionData = userSessionData ?? throw new ArgumentNullException(nameof(userSessionData));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public IUserSessionData UserSessionData { get; }
+}
