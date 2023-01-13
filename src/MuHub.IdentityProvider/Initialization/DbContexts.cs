a89ce18b2a74ba270a08a3df8ff26508a7cf2cd9
strivitech <strivitech@gmail.com>
2023-01-13 23:44:42 +0200

    Add auto migration on app IdP start

---
+++ src/MuHub.IdentityProvider/Initialization/DbContexts.cs
@@ -0,0 +1,32 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+
+using Microsoft.EntityFrameworkCore;
+
+using MuHub.IdentityProvider.Data;
+
+using Serilog;
+
+namespace MuHub.IdentityProvider.Initialization;
+
+public static class DbContexts
+{
+    public static void Migrate(IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
+        var configurationDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        var persistedGrantDbContext = serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+        var applicationDbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            configurationDbContext.Database.Migrate();
+            persistedGrantDbContext.Database.Migrate();
+            applicationDbContext.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while migrating the databases");
+            throw;
+        }
+    }
+}
