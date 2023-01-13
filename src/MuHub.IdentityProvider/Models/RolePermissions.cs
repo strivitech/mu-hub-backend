8f6dc191a9ba6ae86ba16d93f722fecf4a93c840
strivitech <strivitech@gmail.com>
2023-01-13 23:43:39 +0200

    Add role-permissions table

---
+++ src/MuHub.IdentityProvider/Models/RolePermissions.cs
@@ -0,0 +1,10 @@
+﻿namespace MuHub.IdentityProvider.Models;
+
+public class RolePermissions
+{
+    public int Id { get; set; }
+
+    public string RoleName { get; set; }
+    
+    public string PermissionsString { get; set; }
+}
