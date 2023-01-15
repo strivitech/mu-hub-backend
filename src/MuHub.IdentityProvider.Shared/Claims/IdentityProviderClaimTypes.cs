cff988d75f55c8f76a08ff9d6875b5fd4e1d6182
strivitech <strivitech@gmail.com>
2023-01-15 15:02:36 +0200

    Update shared claim types so some of them reference to JwtClaimTypes (IdentityModel)

--- src/MuHub.IdentityProvider.Shared/Claims/IdentityProviderClaimTypes.cs
+++ src/MuHub.IdentityProvider.Shared/Claims/IdentityProviderClaimTypes.cs
@@ -1,16 +1,18 @@
-﻿namespace MuHub.IdentityProvider.Shared.Claims;
+﻿using IdentityModel;
+
+namespace MuHub.IdentityProvider.Shared.Claims;
 
 public class IdentityProviderClaimTypes
 {
     /// <summary>
     /// Contains the 'sub' name.
     /// </summary>
-    public const string Sub = "sub";
+    public const string Sub = JwtClaimTypes.Subject;
 
     /// <summary>
     /// Contains the 'role' name.
     /// </summary>
-    public const string Role = "role";
+    public const string Role = JwtClaimTypes.Role;
 
     /// <summary>
     /// Contains the 'permissions' name.
