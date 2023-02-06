389562471b1af85b7021376741b60b0c07068be3
strivitech <strivitech@gmail.com>
2023-02-07 00:28:46 +0200

    Add coin model

---
+++ src/CoinGecko.Api/Features/Coins/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoinGecko.Api.Features.Coins;
+
+public class Coin
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = null!;
+
+    [JsonPropertyName("symbol")]
+    public string Symbol { get; set; } = null!;
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyName("platforms")]
+    public Dictionary<string,string>? Platforms { get; set; }
+}
