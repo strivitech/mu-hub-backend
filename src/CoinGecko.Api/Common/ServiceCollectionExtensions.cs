829bf3e0b66d751ddd4963c58be01c42a54bb140
strivitech <strivitech@gmail.com>
2023-02-07 00:30:54 +0200

    Add convenient setup method for service collection

---
+++ src/CoinGecko.Api/Common/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CoinGecko.Api.Endpoints;
+using CoinGecko.Api.Features.Coins;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace CoinGecko.Api.Common;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCoinGeckoApi(this IServiceCollection services)
+    {
+        services.AddScoped<ICoinsDataProvider, CoinsDataProvider>(); // TODO: add factory
+        services.TryAddTransient<IRequestCoordinator, RequestCoordinator>();
+        services.AddHttpClient<IRequestCoordinator, RequestCoordinator>(client =>
+        {
+            client.BaseAddress = GeneralEndpointsInfo.ApiUri;
+        });
+        return services;
+    }
+}
