5a4235087dae16845076d853029c020e0e7ef4e9
strivitech <strivitech@gmail.com>
2023-02-10 20:27:28 +0200

    Add Polly retries

---
+++ src/CoinGecko.Api/Common/RequestPolly.cs
@@ -0,0 +1,7 @@
+﻿namespace CoinGecko.Api.Common;
+
+public static class RequestPolly
+{
+    public const int MedianFirstRetryDelaySeconds = 1;
+    public const int DefaultRetryCount = 3;
+}
--- src/CoinGecko.Api/Common/ServiceCollectionExtensions.cs
+++ src/CoinGecko.Api/Common/ServiceCollectionExtensions.cs
@@ -4,18 +4,32 @@ using CoinGecko.Api.Features.Coins;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+
 namespace CoinGecko.Api.Common;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddCoinGeckoApi(this IServiceCollection services)
     {
-        services.AddScoped<ICoinsDataProvider, CoinsDataProvider>(); // TODO: add factory
-        services.TryAddTransient<IRequestCoordinator, RequestCoordinator>();
+        ArgumentNullException.ThrowIfNull(services);
+        services.AddScoped<ICoinsDataProvider, CoinsDataProvider>();
+        services.TryAddScoped<IRequestCoordinator, RequestCoordinator>();
+        services.AddHttpClientForRequestCoordinator();
+        return services;
+    }
+
+    public static IServiceCollection AddHttpClientForRequestCoordinator(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
         services.AddHttpClient<IRequestCoordinator, RequestCoordinator>(client =>
         {
             client.BaseAddress = GeneralEndpointsInfo.ApiUri;
-        });
-        return services;
+        }).AddTransientHttpErrorPolicy(
+        policyBuilder => policyBuilder.WaitAndRetryAsync(
+                Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(RequestPolly.MedianFirstRetryDelaySeconds),
+                    RequestPolly.DefaultRetryCount)));
+            return services;
     }
 }
