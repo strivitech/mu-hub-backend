88b56f58cf6571d4c19e4e4d2f0819689671dd4d
strivitech <strivitech@gmail.com>
2023-02-17 11:53:02 +0200

    Add Proxy layer for market API calls

---
+++ src/MuHub.Market.Proxy/Features/Coins/ICoinsDataService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+
+namespace MuHub.Market.Proxy.Features.Coins;
+
+/// <summary>
+/// Coins data service.
+/// </summary>
+public interface ICoinsDataService
+{
+    /// <summary>
+    /// Asynchronously gets the coin list.
+    /// </summary>
+    /// <returns>A <see cref="Result"/> of <see cref="List{T}"/> of <see cref="Coin"/>.</returns>
+    Task<Result<List<Coin>?>> GetCoinListAsync();
+}
---
+++ src/MuHub.Market.Proxy/Mapping/CoinToGeckoCoin.cs
@@ -0,0 +1,34 @@
+﻿using MuHub.Market.Proxy.Features.Coins;
+
+namespace MuHub.Market.Proxy.Mapping;
+
+/// <summary>
+/// Mapping from CoinGecko coin to MuHub coin.
+/// </summary>
+public static class CoinToGeckoCoin
+{
+    /// <summary>
+    /// Maps from CoinGecko coin to MuHub coin.
+    /// </summary>
+    /// <param name="coin">Coin.</param>
+    /// <returns><see cref="Coin"/>.</returns>
+    public static Coin ToCoin(this CoinGecko.Api.Features.Coins.Coin coin)
+    {
+        return new Coin
+        {
+            Id = coin.Id,
+            Name = coin.Name,
+            Symbol = coin.Symbol,
+        };
+    }
+    
+    /// <summary>
+    /// Maps from CoinGecko coins list to MuHub coins list.
+    /// </summary>
+    /// <param name="coins">Coins.</param>
+    /// <returns><see cref="List{T}"/> of <see cref="Coin"/>.</returns>
+    public static List<Coin> ToCoins(this IEnumerable<CoinGecko.Api.Features.Coins.Coin> coins)
+    {
+        return coins.Select(ToCoin).ToList();
+    }
+}
