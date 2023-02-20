67740be24d092947edd1e9727b1e581a92e37ce0
strivitech <strivitech@gmail.com>
2023-02-20 21:49:05 +0200

    Add paged list

---
+++ src/MuHub.Application/Structures/PagedList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MuHub.Application.Structures;
+
+public class PagedList<T> : IReadOnlyList<T>
+{
+    private readonly IList<T> _items;
+
+    public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        _items = items as IList<T> ?? new List<T>(items);
+    }
+
+    public int PageSize { get; }
+
+    public int TotalPages { get; }
+
+    public int PageNumber { get; }
+
+    public bool IsFirstPage => PageNumber == 1;
+
+    public bool IsLastPage => PageNumber == TotalPages;
+
+    public bool HasPreviousPage => PageNumber > 1;
+
+    public bool HasNextPage => PageNumber < TotalPages;
+
+    public int Count => _items.Count;
+
+    public T this[int index] => _items[index];
+
+    public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize,
+        CancellationToken cancellationToken = default)
+    {
+        var count = await source.CountAsync(cancellationToken);
+        var items = new List<T>();
+        if (count > 0)
+        {
+            items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
+        }
+
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
+}
