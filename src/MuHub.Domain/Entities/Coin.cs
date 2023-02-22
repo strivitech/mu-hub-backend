dd2e4ea6b0baad24bd3a1b42736f9d6b518b7aaa
strivitech <strivitech@gmail.com>
2023-02-23 00:46:05 +0200

    Add Coin, MarketCoin to domain entities

---
+++ src/MuHub.Domain/Entities/Coin.cs
@@ -0,0 +1,22 @@
+﻿using MuHub.Domain.Common.Aggregates;
+using MuHub.Domain.Common.Entities;
+
+namespace MuHub.Domain.Entities;
+
+public class Coin : BaseEntity<int>, IAggregateRoot
+{
+    /// <summary>
+    /// Symbol id.
+    /// </summary>
+    public string SymbolId { get; set; } = null!;
+
+    /// <summary>
+    /// Symbol.
+    /// </summary>
+    public string Symbol { get; set; } = null!;
+
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public string Name { get; set; } = null!;
+}
---
+++ src/MuHub.Domain/Entities/MarketCoin.cs
@@ -0,0 +1,23 @@
+﻿using MuHub.Domain.Common.Aggregates;
+using MuHub.Domain.Common.Entities;
+
+namespace MuHub.Domain.Entities;
+
+public class MarketCoin : BaseEntity<int>, IAggregateRoot
+{
+    public string SymbolId { get; set; } = null!;
+
+    public string Symbol { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+    
+    public decimal CurrentPrice { get; set; } 
+
+    public decimal? High24H { get; set; }
+
+    public decimal? Low24H { get; set; }
+    
+    public DateTimeOffset LastUpdated { get; set; }
+}
