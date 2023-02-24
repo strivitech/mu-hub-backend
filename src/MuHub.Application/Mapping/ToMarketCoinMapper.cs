4f0c5d3ed09901de81d23e58dd192177a5a7e0d9
strivitech <strivitech@gmail.com>
2023-02-24 18:34:12 +0200

    Add ToMarketCoin mappings

---
+++ src/MuHub.Application/Mapping/ToMarketCoinMapper.cs
@@ -0,0 +1,27 @@
+﻿using MuHub.Domain.Entities;
+using MuHub.Market.Proxy.Features.Coins;
+
+namespace MuHub.Application.Mapping;
+
+public static class ToMarketCoinMapper
+{
+    public static MarketCoin ToMarketCoin(this MarketCoinDto dto)
+    {
+        return new MarketCoin
+        {
+            SymbolId = dto.Id,
+            Symbol = dto.Symbol,
+            Name = dto.Name,
+            ImageUrl = dto.ImageUrl,
+            CurrentPrice = dto.CurrentPrice,
+            High24H = dto.High24H,
+            Low24H = dto.Low24H,
+            LastUpdated = dto.LastUpdated,
+        };
+    }
+
+    public static List<MarketCoin> ToMarketCoins(this IEnumerable<MarketCoinDto> dtos)
+    {
+        return dtos.Select(ToMarketCoin).ToList();
+    }
+}
