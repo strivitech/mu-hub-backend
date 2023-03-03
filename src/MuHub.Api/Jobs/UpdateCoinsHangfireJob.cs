52718e7d49c15e5ed5057f38a18de4502f80cd05
strivitech <strivitech@gmail.com>
2023-03-04 00:41:25 +0200

    Update coin information logic

--- src/MuHub.Api/Jobs/UpdateCoinsHangfireJob.cs
+++ src/MuHub.Api/Jobs/UpdateCoinsHangfireJob.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Collections.Immutable;
+
+using Microsoft.AspNetCore.SignalR;
 
 using MuHub.Api.Hubs;
 using MuHub.Api.Services;
 using MuHub.Application.Contracts.Persistence;
+using MuHub.Application.Exceptions;
 using MuHub.Market.Proxy.Features.Coins;
 
 namespace MuHub.Api.Jobs;
@@ -12,15 +15,21 @@ namespace MuHub.Api.Jobs;
 /// </summary>
 public class UpdateCoinsHangfireJob
 {
+    private readonly ICoinsDataService _coinsDataService;
+    private readonly ICoinsStorage _coinsStorage;
     private readonly IMarketCoinsInteractionService _marketCoinsInteractionService;
 
     /// <summary>
     /// 
     /// </summary>
     /// <param name="marketCoinsInteractionService"></param>
-    public UpdateCoinsHangfireJob(IMarketCoinsInteractionService marketCoinsInteractionService)
+    /// <param name="coinsStorage"></param>
+    public UpdateCoinsHangfireJob(IMarketCoinsInteractionService marketCoinsInteractionService,
+        ICoinsDataService coinsDataService, ICoinsStorage coinsStorage)
     {
         _marketCoinsInteractionService = marketCoinsInteractionService;
+        _coinsDataService = coinsDataService;
+        _coinsStorage = coinsStorage;
     }
 
     /// <summary>
@@ -28,6 +37,13 @@ public class UpdateCoinsHangfireJob
     /// </summary>
     public async Task UpdateCoinInformation()
     {
-        await _marketCoinsInteractionService.UpdateCoinInformation();
+        var coins = await _coinsStorage.GetAllAsync();
+
+        if (!coins.Any())
+        {
+            throw new UnexpectedException("No coins found to update information");
+        }
+
+        await _marketCoinsInteractionService.UpdateCoinInformation(coins.Select(x => x.SymbolId).ToImmutableArray());
     }
 }
