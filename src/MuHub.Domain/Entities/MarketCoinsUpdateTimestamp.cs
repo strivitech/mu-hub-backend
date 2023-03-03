5d9150879cdaa415804b03bcfc0900eda2310e66
strivitech <strivitech@gmail.com>
2023-03-04 00:37:39 +0200

    Add MarketCoinsUpdateTimestamp

---
+++ src/MuHub.Domain/Entities/MarketCoinsUpdateTimestamp.cs
@@ -0,0 +1,9 @@
+﻿using MuHub.Domain.Common.Aggregates;
+using MuHub.Domain.Common.Entities;
+
+namespace MuHub.Domain.Entities;
+
+public class MarketCoinsUpdateTimestamp : BaseEntity<int>, IAggregateRoot
+{
+    public DateTimeOffset LastUpdated { get; set; }
+}
