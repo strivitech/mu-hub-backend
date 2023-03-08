f1c425fc103efb6f45a5b01fe3af04280b6a6293
strivitech <strivitech@gmail.com>
2023-03-08 11:58:34 +0200

    Change key type for update timestamp

--- src/MuHub.Domain/Entities/MarketCoinsUpdateTimestamp.cs
+++ src/MuHub.Domain/Entities/MarketCoinsUpdateTimestamp.cs
@@ -3,7 +3,7 @@ using MuHub.Domain.Common.Entities;
 
 namespace MuHub.Domain.Entities;
 
-public class MarketCoinsUpdateTimestamp : BaseEntity<int>
+public class MarketCoinsUpdateTimestamp : BaseEntity<long>
 {
     public DateTimeOffset LastUpdated { get; set; }
 }
