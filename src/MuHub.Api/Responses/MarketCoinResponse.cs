82a5101b47063108d9df20010e3aad20b3c4208f
strivitech <strivitech@gmail.com>
2023-03-18 22:58:10 +0200

    Simplify records

--- src/MuHub.Api/Responses/MarketCoinResponse.cs
+++ src/MuHub.Api/Responses/MarketCoinResponse.cs
@@ -12,23 +12,4 @@ public record MarketCoinResponse(
     string? ImageUrl,
     decimal? High24H,
     decimal? Low24H,
-    bool IsValid)
-{
-    public string Id { get; } = Id;
-    
-    public string Name { get; } = Name;
-    
-    public string Symbol { get; } = Symbol;
-
-    public string? ImageUrl { get; } = ImageUrl;
-
-    public decimal? CurrentPrice { get; } = CurrentPrice;
-    
-    public decimal? High24H { get; } = High24H;
-
-    public decimal? Low24H { get; } = Low24H;
-
-    public DateTimeOffset? LastUpdated { get; } = LastUpdated;
-
-    public bool IsValid { get; } = IsValid;
-}
+    bool IsValid);
--- src/MuHub.Market.Proxy/Features/Coins/MarketCoinDto.cs
+++ src/MuHub.Market.Proxy/Features/Coins/MarketCoinDto.cs
@@ -8,21 +8,4 @@ public record MarketCoinDto(
     string? ImageUrl,
     decimal? High24H,
     decimal? Low24H,
-    bool IsValid)
-{
-    public string Id { get; } = Id;
-
-    public string? ImageUrl { get; } = ImageUrl;
-
-    public decimal? CurrentPrice { get; } = CurrentPrice;
-    
-    public int? MarketCapRank { get; } = MarketCapRank;
-
-    public decimal? High24H { get; } = High24H;
-
-    public decimal? Low24H { get; } = Low24H;
-
-    public DateTimeOffset? LastUpdated { get; } = LastUpdated;
-
-    public bool IsValid { get; } = IsValid;
-}
+    bool IsValid);
