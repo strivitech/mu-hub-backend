27df4993d60f9af6c301d2ca1089d78d7872da17
strivitech <strivitech@gmail.com>
2023-06-11 13:43:19 +0300

    Change permissions

--- src/MuHub.Permissions/Permission.cs
+++ src/MuHub.Permissions/Permission.cs
@@ -7,15 +7,27 @@ namespace MuHub.Permissions;
 /// </summary>
 public enum Permission
 {
-    [Display(GroupName = "Info", Name = "Read", Description = "Allowed to read Info")]
-    InfoRead = 1,
+    // [Display(GroupName = "Info", Name = "Read", Description = "Allowed to read Info")]
+    // InfoRead = 1,
+    //
+    // [Display(GroupName = "Info", Name = "Create", Description = "Allowed to create Info")]
+    // InfoCreate = 2,
+    //
+    // [Display(GroupName = "Info", Name = "Update", Description = "Allowed to update Info")]
+    // InfoUpdate = 3,
+    //
+    // [Display(GroupName = "Info", Name = "Delete", Description = "Allowed to delete Info")]
+    // InfoDelete = 4,
     
-    [Display(GroupName = "Info", Name = "Create", Description = "Allowed to create Info")]
-    InfoCreate = 2,
+    [Display(GroupName = "WatchList", Name = "Read", Description = "Allowed to read WatchList")]
+    WatchListRead = 1,
     
-    [Display(GroupName = "Info", Name = "Update", Description = "Allowed to update Info")]
-    InfoUpdate = 3,
+    [Display(GroupName = "WatchList", Name = "Create", Description = "Allowed to create WatchList")]
+    WatchListCreate = 2,
     
-    [Display(GroupName = "Info", Name = "Delete", Description = "Allowed to delete Info")]
-    InfoDelete = 4,
+    [Display(GroupName = "WatchList", Name = "Update", Description = "Allowed to update WatchList")]
+    WatchListUpdate = 3,
+    
+    [Display(GroupName = "WatchList", Name = "Delete", Description = "Allowed to delete WatchList")]
+    WatchListDelete = 4,
 }
