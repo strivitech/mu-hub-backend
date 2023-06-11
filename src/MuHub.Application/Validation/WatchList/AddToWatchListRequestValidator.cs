86807beb4ff21b7385680079505a2945c78cfe04
strivitech <strivitech@gmail.com>
2023-06-11 13:49:51 +0300

    Create AddToWatchListRequestValidator

---
+++ src/MuHub.Application/Validation/WatchList/AddToWatchListRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+using MuHub.Application.Models.Data.WatchList;
+
+namespace MuHub.Application.Validation.WatchList;
+
+public class AddToWatchListRequestValidator : AbstractValidator<AddToWatchListRequest>
+{
+    public AddToWatchListRequestValidator()
+    {
+        RuleFor(x => x.CoinId)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("CoinId must be greater than or equal to 1.");
+    }
+}
