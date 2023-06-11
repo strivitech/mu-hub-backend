a5961cb908813c70b90669a775f2625293292bc9
strivitech <strivitech@gmail.com>
2023-06-11 13:41:45 +0300

    Update database context. Add new migrations

---
+++ src/MuHub.Infrastructure/Persistence/Migrations/20230607175834_AddWatchLists.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MuHub.Infrastructure.Persistence.Migrations
+{
+    public partial class AddWatchLists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WatchLists",
+                columns: table => new
+                {
+                    CoinId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchLists", x => new { x.CoinId, x.UserId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WatchLists");
+        }
+    }
+}
